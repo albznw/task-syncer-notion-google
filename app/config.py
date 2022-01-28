e6e738a8ed0a1c7ba19931ca6d052029b7955682
Albin Winkelmann <albin-w@hotmail.com>
2022-01-28 11:41:02 +0100

    Update config with new env variables

--- app/config.py
+++ app/config.py
@@ -1,17 +1,15 @@
 from pydantic import BaseSettings
 
-
 class Settings(BaseSettings):
     app_name: str = "TaskSyncer Notion-Google"
     notion_secret: str
-    noition_task_db: str
-    # google_secret: str
+    notion_task_db: str
+    notion_bucket_db: str
+    mongo_db_username: str
+    mongo_db_password: str
 
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
-
-# if settings.notion_secret == "" or settings.google_secret == "":
-    # raise EnvironmentError("You need to specify tokens")

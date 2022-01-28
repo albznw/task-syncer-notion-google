f69128b4b2128352c20bd39243c8624d75d65f76
Albin Winkelmann <albin-w@hotmail.com>
2022-01-28 11:43:36 +0100

    Update main functino

---
+++ main.py
@@ -0,0 +1,12 @@
+from app.sync import notion_to_google_sync, google_to_notion_sync, purge_tasks_google, purge_tasks_notion
+
+if __name__ == "__main__":
+    print("Syncing...")
+    google_to_notion_sync()
+    google_to_notion_sync()
+    purge_tasks_notion()
+    notion_to_google_sync()
+    notion_to_google_sync()
+    purge_tasks_google()
+
+exit(0)

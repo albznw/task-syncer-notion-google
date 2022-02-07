9c2667aed07af0d84c7ed7845169d516d0ab7d0a
Albin Winkelmann <albin-w@hotmail.com>
2022-02-07 01:26:05 +0100

    Add GoogleTaskRepository

--- app/models/mongo.py
+++ app/models/mongo.py
@@ -5,6 +5,7 @@ from mongomantic import connect as connect_mongo
 
 from app.config import settings
 from app.models.notion import NotionTask
+from app.models.google import GoogleTask
 
 # Setup MongoDB connection
 mongo_uri = f"mongodb://{quote_plus(settings.mongo_username)}:{quote_plus(settings.mongo_password)}@{settings.mongo_url}"
@@ -13,6 +14,14 @@ connect_mongo(mongo_uri, settings.mongo_db)
 
 
 class NotionTaskRepository(BaseRepository):
+
     class Meta:
         model = NotionTask
         collection = "notion-task"
+
+
+class GoogleTaskRepository(BaseRepository):
+    
+    class Meta:
+        model = GoogleTask
+        collection = "google-task"

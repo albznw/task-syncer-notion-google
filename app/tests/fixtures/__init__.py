67ea5ec0792bad9a0233a1457594e94dd53c4a8a
Albin Winkelmann <albin-w@hotmail.com>
2022-02-07 01:25:17 +0100

    Update Notion tests

--- app/tests/fixtures/__init__.py
+++ app/tests/fixtures/__init__.py
@@ -7,7 +7,7 @@ from app.config import settings
 mongo_uri = f"mongodb://{quote_plus(settings.mongo_username)}:{quote_plus(settings.mongo_password)}@{settings.mongo_url}"
 
 @pytest.fixture()
-def db_fixture():
+def mongo_fixture():
     print("setup")
     db_client = MongoClient(mongo_uri)
     yield
--- app/tests/models/test_notion_mongo_pytest.py
+++ app/tests/models/test_notion_mongo_pytest.py
@@ -1,16 +1,32 @@
-from app.models.mongo import NotionTaskRepository
-from app.models.notion import NotionTask, NotionTaskDB
-from app.tests.fixtures import db_fixture
+from app.models.mongo import GoogleTaskRepository, NotionTaskRepository
+from app.tests.fixtures import mongo_fixture
+from app.models.notion import NotionTask, NotionTasks
+from app.models.google import GoogleStatus, GoogleTask
+from app.config import settings
+
+class TestNotionTaskRepository:
+    def test_mongo(self, mongo_fixture):
+        task_before_save = next(NotionTasks().list())
+        NotionTaskRepository.save(task_before_save)
+
+        task_gen = NotionTaskRepository.find()
+        mongo_tasks = [t for t in task_gen]
+
+        assert len(mongo_tasks) == 1
+        assert isinstance(mongo_tasks[0], NotionTask)
+        assert mongo_tasks[0].dict(exclude={"id"}) == task_before_save.dict(exclude={"id"})
 
 
-def test_mongo(db_fixture):
-    task_before_save = next(NotionTaskDB().list())
-    NotionTaskRepository.save(task_before_save)
+class TestGoogleTaskRepository:
+    def test_mongo(self, mongo_fixture):
+        # Create Google task
+        task_before_save = GoogleTask(
+            title="Testtask",
+            status=GoogleStatus.todo,
+            tasklist=settings.google_default_tasklist
+        ).google_save()
 
+        saved_task = GoogleTaskRepository.save(task_before_save)
 
-    task_gen = NotionTaskRepository.find()
-    mongo_tasks = [t for t in task_gen]
-
-    assert len(mongo_tasks) == 1
-    assert isinstance(mongo_tasks[0], NotionTask)
-    assert task_before_save.dict(exclude={"id"}) == mongo_tasks[0].dict(exclude={"id"})
+        assert isinstance(saved_task, GoogleTask)
+        assert saved_task.dict(exclude={"id"}) == task_before_save.dict(exclude={"id"})
